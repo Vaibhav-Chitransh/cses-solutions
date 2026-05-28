3296385f38b36456ba386fd0731da9b15496454a
Vaibhav-Chitransh <vaibhavchitranshbdn@gmail.com>
2026-05-28 21:18:26 +0530

    coin combinations 1 solution added

---
+++ Dynamic Programming/coin_combinations_1.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+const int MOD = 1000000007;
+ 
+int main(){
+    int n, x;
+    cin >> n >> x;
+ 
+    vector<int> coins(n);
+    for(int i=0; i<n; i++) cin >> coins[i];
+ 
+    vector<int> dp(x+1);
+    dp[0] = 1;
+ 
+    for(int i=1; i<=x; i++) {
+        for(int j=0; j<n; j++) {
+            if(i - coins[j] >= 0) {
+                dp[i] = (dp[i] + dp[i - coins[j]]) % MOD;
+            }
+        }
+    }
+ 
+    cout << dp[x];
+ 
+    return 0;
+}
